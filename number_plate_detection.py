5b4d3256126894db6ba0189c1e5bfa10506303bd
Mohit Kumar <53662024+moit-bytes@users.noreply.github.com>
2020-03-24 22:38:47 +0530

    Added driver code

---
+++ number_plate_detection.py
@@ -0,0 +1,20 @@
+import requests
+import base64
+import json
+
+def number_plate(image_path):
+    secret_key = 'sk_49a53da40c924e10d4e086b0'
+    with open(image_path, 'rb') as image_file:
+            img_base64 = base64.b64encode(image_file.read())
+
+    url = 'https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=ind&secret_key=%s' % (secret_key)
+    r = requests.post(url, data = img_base64)
+    try:
+        print("Car number = ", r.json()['results'][0]['plate'])
+        print("Model Colour = ", r.json()['results'][0]['vehicle']['color'][0]['name'])
+        print("Car body_type = ", r.json()['results'][0]['vehicle']['body_type'][0]['name'])
+        print("Car Maker = ", r.json()['results'][0]['vehicle']['make'][0]['name'])
+        print("Car Model = ", r.json()['results'][0]['vehicle']['make_model'][0]['name'])
+        
+    except:
+        print("No number plate found")
